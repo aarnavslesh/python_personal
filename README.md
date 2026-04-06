e000a9b020342787fb46bd4ad75fa35d3e1b2a36
aarnavslesh <shailendrakumar@aarnavs-macbook-pro.local>
2026-04-06 07:26:44 +0530

    created playback function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python_personal
+
+
+
+project to build a basic music player in python, with functions like play/pause, skip, previous, forward and a basic user frontend using pygame.
--- music_backend.py
+++ music_backend.py
@@ -1,1 +1,13 @@
-a = print("hello")
+import pygame
+from mutagen.mp3 import MP3
+pygame.init()
+
+#database?
+#playback functions
+def play_song(song_id):
+    pygame.mixer.music.load(song_id)
+    pygame.mixer.music.play()
+
+def pause_song()
+def next_song()
+def previous_song()
